1c6d0f4171c22518cb80e741292f3ef1e8ce49f6
kartik-giri <kartikgiri1t30@gmail.com>
2026-09-15 18:09:39 +0530

    deploying

--- apps/inkflowapp/components/canvas/RoomCanvas.tsx
+++ apps/inkflowapp/components/canvas/RoomCanvas.tsx
@@ -6,8 +6,8 @@ const RoomCanvas = ({roomId}: {roomId:number})=>{
     const [socket, setSocket] = useState<WebSocket| null>(null)
     
     useEffect(()=>{
-        //  const ws = new WebSocket("ws://ec2-100-51-65-9.compute-1.amazonaws.com:8080") //cooke will go automatically with every subsequent http request
-        const ws = new WebSocket(process.env.WEBSOCKET_URL!) //cooke will go automatically with every subsequent http request
+         const ws = new WebSocket("ws://ec2-100-51-65-9.compute-1.amazonaws.com:8080") //cooke will go automatically with every subsequent http request
+        // const ws = new WebSocket(process.env.WEBSOCKET_URL!) //cooke will go automatically with every subsequent http request
         
         ws.onopen = ()=>{
             setSocket(ws);
--- apps/inkflowapp/components/ui/WsConnect.tsx
+++ apps/inkflowapp/components/ui/WsConnect.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react"
 export const WsConnect = ()=>{
     const [socket, setSocket] = useState<WebSocket | null>();
     useEffect(()=>{
-        const ws = new WebSocket(process.env.WEBSOCKET_URL!)
+        const ws = new WebSocket("ws://ec2-100-51-65-9.compute-1.amazonaws.com:8080") //cooke will go automatically with every subsequent http request
+        // const ws = new WebSocket(process.env.WEBSOCKET_URL!)
         setSocket(ws);
 
         ws.onopen= ()=>{
